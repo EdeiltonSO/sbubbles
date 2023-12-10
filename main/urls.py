cb92a2cae9902721a318096130026c0244f20681
Edeilton Oliveira <edeiltonso@gmail.com>
2023-12-10 17:37:09 -0300

    fix: corrige erro NoReverseMatch

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import TemplateView
-from app.views import timeline
+from app.views import home
 
 urlpatterns = [
     # path('', include('app.urls')),
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
-    path('', timeline, name='timeline')
+    path('', home, name='home')
 ]
