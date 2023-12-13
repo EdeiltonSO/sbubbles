39847fa8d4bb8e73f13c0c3171033b80ae2615b3
Edeilton Oliveira <edeiltonso@gmail.com>
2023-12-13 02:33:16 -0300

    feat: cria primeira versão da funcionalidade de cadastro

---
+++ app/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from app.models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    birthdate = forms.DateField(
+        label='Data de nascimento',
+        widget=forms.DateInput(attrs={'type': 'date'}),
+        required=False
+    )
+    
+    class Meta:
+        model = CustomUser
+        fields = ('first_name', 'last_name', 'email', 'birthdate', 'username', 'password1', 'password2')
--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.views import generic
+from app.forms import CustomUserCreationForm
     
 @login_required(login_url='login')
 def home(request):
     return render(request, 'home.html')
+
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
